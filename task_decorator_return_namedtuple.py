ceafc59e8d19473dee35b3587f10ce68c83ffb10
AnnaBarmina <anna.barmina@gmail.com>
2018-05-21 00:01:15 +0300

    Dec2

---
+++ task_decorator_return_namedtuple.py
@@ -0,0 +1,19 @@
+from collections import namedtuple
+
+def return_namedtuple(*cort):
+	def decorator(func):
+		def wrapper(*args, **kwargs):
+			result = func(*args)
+			if isinstance(result, tuple):
+				ncort = namedtuple('ncort', cort)
+				namedcort = ncort(*result)
+			return namedcort
+		return wrapper
+	return decorator
+
+@return_namedtuple('a', 'b', 'c')
+def func():
+    return 'Python', 'is', 'programming language'
+
+r = func()
+print(r.c)
