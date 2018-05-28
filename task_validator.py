c2aa55385f9ce5ecaae42ba20fff5904aff74ce2
AnnaBarmina <anna.barmina@gmail.com>
2018-05-28 14:46:41 +0300

    Validator

---
+++ task_validator.py
@@ -0,0 +1,56 @@
+from abc import ABCMeta, abstractmethod
+from datetime import datetime
+
+
+class ValidatorException(Exception):
+	pass
+
+class Validator(metaclass=ABCMeta):
+	types = {}
+
+		@abstractmethod
+	def validate(value):
+		pass
+
+	
+	@classmethod
+	def get_instance(cls, name):
+		klass = cls.types.get(name)
+		if klass is None:
+			raise ValidatorException('Validator with name "{}" not found'.format(name))
+		return klass
+
+	@classmethod
+	def add_type(cls, name, klass):
+		if not name:
+			raise ValidatorException('Validator must have a name!')
+		if not issubclass(klass, Validator):
+			raise ValidatorException('Class "{}" is not Validator!'.format(klass))
+		if name in Validator.types:
+			raise ValidatorException('Class "{}" is already exists'.format(klass))
+		Validator.types[name] = klass
+
+class EMailValidator(Validator):
+	"""проверяет корректность введенного email"""
+	def validate(self, email):
+		if '@' in email:
+			return True
+		else:
+			return False
+
+class DateTimeValidator(Validator):
+	"""проверяет корректтность даты"""
+	def validate(self, datetime):
+		for datetime in ['%Y-%m-%d',
+						'%Y-%m-%d %H:%M',
+						'%Y-%m-%d %H:%M:%S',
+						'%d.%m.%Y',
+						'%d.%m.%Y %H:%M',
+						'%d.%m.%Y %H:%M:%S',
+						'%d/%m/%Y',
+						'%d/%m/%Y %H:%M',
+						'%d/%m/%Y %H:%M:%S']:
+			return True
+		else:
+			return False
+		
