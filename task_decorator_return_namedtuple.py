1459e1d644ca2267d1a1155120ac916f86517b90
AnnaBarmina <anna.barmina@gmail.com>
2018-06-05 19:09:41 +0300

    Vers2

--- task_decorator_return_namedtuple.py
+++ task_decorator_return_namedtuple.py
@@ -1,13 +1,13 @@
 from collections import namedtuple
 
+
 def return_namedtuple(*cort):
 	def decorator(func):
 		def wrapper(*args, **kwargs):
-			result = func(*args)
+			result = func(*args, **kwargs)
 			if isinstance(result, tuple):
-				ncort = namedtuple('ncort', cort)
-				namedcort = ncort(*result)
-			return namedcort
+				ncort = namedtuple('ncort', list(cort))
+			return ncort(*result)
 		return wrapper
 	return decorator
 '''
@@ -17,4 +17,12 @@ def func():
 
 r = func()
 print(r.c)
+
+@return_namedtuple('one', 'two')
+def func():
+    return 1, 2
+
+r = func()
+print(r.one) # 1
+print(r.two) # 2
 '''
