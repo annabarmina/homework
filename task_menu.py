3a4631755f991560e4c4df8eca82de37ac858e19
AnnaBarmina <anna.barmina@gmail.com>
2018-06-06 13:26:51 +0300

    Vers2

--- task_menu.py
+++ task_menu.py
@@ -11,44 +11,28 @@ class Command(metaclass=ABCMeta):
 
 class ShowCommand(Command):
 	def __init__(self, task_id):
-		pass
-
-	def execute(self):
-		pass
+		self.task_id = task_id
 
 class ListCommand(Command):
 	def __init__(self):
-		pass
-
-	def execute(self):
 		pass
 
 class AddCommand(Command):
 	def __init__(self):
 		pass
 
-	def execute(self):
-		pass
-
 class DoneCommand(Command):
 	def __init__(self):
-		pass
-
-	def execute(self):
 		pass
 
 class ReturnedCommand(Command):
 	def __init__(self):
 		pass
 
-	def execute(self):
-		pass
-
-class Menu(object):
+class Menu(metaclass=ABCMeta):
 
 	def __init__(self):
-		self.commands = list()
-		self.klasses = list()
+		self.commands = {}
 		self.counter = 0
 
 	def __iter__(self):
@@ -56,34 +40,42 @@ class Menu(object):
 
 	def __next__(self):
 		if self.counter < len(self.commands):
-			ret_val = self.commands[self.counter],self.klasses[self.counter]
-			self.count += 1
-			return ret_val
+			command = list(self.commands.items())[self.counter]
+			self.counter += 1
+			return command
 		else:
-			self.counter = 0
+			#self.counter = 0
 			raise StopIteration('No more!')
 
 	
-	@classmethod
-	def add_command(cls, name, klass):
+	#@classmethod
+	def add_command(self, name, klass):
 		if not name:
 			raise CommandException('Command must have a name!')
 		if not issubclass(klass, Command):
 			raise CommandException('Class "{}" is not Command!'.format(klass))
-		cls.commands[name] = klass
+		self.commands[name] = klass
 
-	@classmethod
-	def execute(cls, name, *args, **kwargs):
-		klass = cls.commands.get(name)
-		if klass is None:
+	#@classmethod
+	def execute(self, name, *args, **kwargs):
+		command = self.commands.get(name)
+		if command is None:
 			raise CommandException('Command with name "{}" not found'.format(name))
-		return klass.execute(name, *args, **kwargs)
+		return command.execute(args, kwargs)
 
 if __name__ == '__main__':
 	menu = Menu()
-
 	menu.add_command('show', ShowCommand)
 	menu.add_command('done', DoneCommand)
+	menu.add_command('returned', ReturnedCommand)
+	menu.execute('show')
+	menu.execute('show', 1, 2)
+	menu.execute('done')
+	#menu.execute('list')
 
 	for i in menu:
 		print(i)
+
+	for name, command in menu:
+		print(name, command)
+		#print(command)
